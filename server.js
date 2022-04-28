b3d22e0b4d64dbf05143302e49136d5506a8acc2
Alton <altondsilva3010@gmail.com>
2022-04-28 17:06:33 +0530

    last commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const { urlencoded } = require("express");
+const cors = require("cors");
 
 const app = express();
 
@@ -8,6 +9,7 @@ connectDB();
 
 app.use(urlencoded({ extended: true }));
 app.use(express.json({ extended: false })); //bodyParser
+app.use(cors());
 
 app.get("/", (req, res) => res.send("API RUNNING"));
 
